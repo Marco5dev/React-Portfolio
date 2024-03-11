db26c641dd5fa4f6d3fafe0e024ae1a2f0a17f41
Mark Maher <100785809+Marco5dev@users.noreply.github.com>
2024-03-11 21:17:54 +0200

    fix the robots outpot

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemapPlugin from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemapPlugin({
+      pages: [
+        { url: '/', lastmod: new Date().toISOString() },
+      ],
+      baseUrl: 'https://marco5dev.site',
+    }),
+  ],
   server: {
     host: '0.0.0.0',
     port: '8001'
   }
-})
+});
