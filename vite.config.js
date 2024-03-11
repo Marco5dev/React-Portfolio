0d99d662765881a052dae5c2009db3be7550c496
Mark Maher <100785809+Marco5dev@users.noreply.github.com>
2024-03-11 20:22:08 +0200

    Add Robots

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemapPlugin from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sitemapPlugin({
+    
+    pages: [  
+      { url: '/', lastmod: new Date().toISOString() },
+    ],
+    baseUrl: 'https://marco5dev.site',
+    output: 'public/sitemap.xml',
+    
+  }),],
   server: {
     host: '0.0.0.0',
     port: '8001'
